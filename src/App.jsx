ab4e3d75b2be2748fd1920991b8c73eac6940d9c
Hasnain-7860 <154454618+Hasnain-7860@users.noreply.github.com>
2025-07-01 21:17:53 +0530

    Homepage complete

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Home from './pages/Home/Home'
+import Navbar from './components/Navbar/Navbar'
+
+const App = () => {
+  return (
+    <div>
+       <Home/>
+      
+      
+    </div>
+  )
+}
+
+export default App
